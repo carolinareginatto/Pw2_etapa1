f412448638fc34e96c9d891926e8e6477fd9695f
carolinareginatto <carolinasreginatto@gmail.com>
2023-02-09 20:51:47 -0300

    Aula 09 Módulos e projetos

---
+++ Aula0602/index.js
@@ -0,0 +1,6 @@
+const meuModulo = require('./soma')
+meuModulo.calculadora(125, 25,"+");
+meuModulo.calculadora(125, 25,"-");
+meuModulo.calculadora(125, 25,"*");
+meuModulo.calculadora(125, 25,"/");
+
---
+++ Aula0602/soma.js
@@ -0,0 +1,18 @@
+module.exports = {
+calculadora (a, b, op){
+    if(op==="+"){
+        console.log(`${a} ${op} ${b} = ${a+b} `)
+    }
+    else if(op==="-"){
+            console.log(`${a} ${op} ${b} = ${a-b} `)
+    }
+    else if(op==="*"){
+        console.log(`${a} ${op} ${b} = ${a*b} `)
+    }
+    else {
+        console.log(`${a} ${op} ${b} = ${a/b} `)
+    }
+
+}    
+
+}
